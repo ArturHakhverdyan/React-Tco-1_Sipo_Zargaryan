228eac4a09b4234e8c976cc83d3e5e3bbbde9559
ArturHakhverdyan <arthurhakhverdyan1@gmail.com>
2022-05-19 10:53:32 -0700

    das 12

--- src/components/Project/FilterSection/index.jsx
+++ src/components/Project/FilterSection/index.jsx
@@ -1,6 +1,12 @@
 import './styles.css'
+// import DatePicker from "react-datepicker";
+
+
 export const FilterSection = () => {
   return <div className="filter-section">
-    
+    {/* <DatePicker/>
+    <DatePicker/>
+    <DatePicker/>
+    <DatePicker/> */}
   </div>;
 };
--- src/components/Project/MainSection/Head/HeadRight/index.jsx
+++ src/components/Project/MainSection/Head/HeadRight/index.jsx
@@ -1,25 +1,30 @@
 import { Input, Button } from "reactstrap";
 import "./styles.css";
-import { useState } from "react"
+import {  useState } from "react"
 import { SharedModal } from "../../../../../shared/sharedModal";
+import { BACKEND_URL } from "../../../../../consts";
 
 
-const SortSelect = () => {
+const SortSelect = ({setTasks,onSortHandler}) => {
+
+
   return (
-    <Input name="sort_by" type="select">
-      <option>Sort By</option>
-      <option>Newest First</option>
-      <option>Oldest First</option>
-      <option>Todo at Newest</option>
+    <Input name="sort_by" type="select" onChange={onSortHandler}>
+      <option value="creation_date_newest" >created newest</option>
+      <option value="creation_date_oldest">Created oldest</option>
+      <option value="completion_date_newest">completed newest</option>
+      <option value="completion_date_oldest">completed oldest</option>
+      <option value="a-z">A-Z</option>
+      <option value="z-a">Z-A</option>
     </Input>
   );
 };
 
-const SearchInput = () => {
-  return <Input type="search" placeholder="Search" name="search"></Input>;
-};
+
 
 export const HeadRight = ({setTasks}) => {
+
+
 
   const [isShowAddTaskModal, setIsShowAddTaskModal] = useState(false);
   const handleBtnClick = () => {
@@ -30,6 +35,31 @@ export const HeadRight = ({setTasks}) => {
     }
   }
 
+  ////dasi jamin
+  const [searchValue,setSearchValue] = useState(null)
+  const [sortValue,setSortValue] = useState(null)
+
+
+  const onSearch = (e) => {
+    setSearchValue(e.target.value)
+    fetch(`${BACKEND_URL}/task?search=${e.target.value}`)
+    .then((res ) => res.json())
+    .then((data) => setTasks(data))
+
+ }
+
+ const onSortHandler = (e)=> {
+  const {value} = e.target
+
+
+  fetch(`${BACKEND_URL}/task?sort=${value}`)
+  .then(res=>res.json())
+  .then(data=> setTasks(data))
+}
+
+
+
+
   return (
     <div className="main-section-head-right">
       <Button style={{ width: "100%" }}
@@ -39,8 +69,8 @@ export const HeadRight = ({setTasks}) => {
       >
         Add New Task
       </Button>
-      <SortSelect />
-      <SearchInput />
+      <SortSelect setTasks={setTasks}  onSortHandler={onSortHandler}/>
+      <Input type="search" placeholder="Search" name="search" onChange={onSearch}></Input>;
       {isShowAddTaskModal && (<SharedModal
         onClose={() => {
           setIsShowAddTaskModal(false)
